9aee2bd4cad1eab277d437d5b41b0b69edcafa74
tadeo meggiolaro <tadeo.meggiolaro@gmail.com>
2024-05-18 18:10:39 -0300

    FirstCommit.

---
+++ javascript/scriptIndex.js
@@ -0,0 +1,45 @@
+
+let primerNombreApellido = prompt("Ingrese su nombre y apellido");
+let prestamo = prompt ("Ingrese monto a solicitar hasta $30.000");
+
+while (prestamo < 100 || prestamo > 30000){
+    prestamo = prompt ("Ingrese monto a solicitar hasta $30.000");
+};
+const MENSAJE = validacionPrestamo(prestamo);
+alert(MENSAJE)
+
+function validacionPrestamo (prestamo){
+    if (prestamo == 30000) return "Felicitaciones!! " +primerNombreApellido + " Es el prestamo mas grande que hace la empresa queremos felicitarlo y beneficiarlo con ademas de 24 cuotas,tenga 36 cuotas por si elije devolver el dinero de esa forma"
+    if (prestamo < 30000 && prestamo >= 20000 ) return "Hola sr. " + primerNombreApellido +". Usted solicito un prestamo de " + prestamo + " y tiene para devolver el dinero en 12 o 24 coutas"
+    if (prestamo <= 10000 && prestamo >= 100) return "Hola sr. " + primerNombreApellido +". Usted solicito un prestamo de " + prestamo + " y tiene para devolver el dinero en 12"
+};
+
+// Conceptos que entran en la 1er Preentrega
+    // - Variables
+    
+    // - Constantes
+    
+    // - Booleanos
+    
+    // - Prompt
+    
+    // - Alert
+    
+    // - Console.log
+    
+    // - Control de flujo con if y if else.
+    
+    // - Operadores logicos
+    
+    // - Ciclos e iteraciones usando:
+    
+    // For
+    // While
+    // do While
+    
+    
+    // - Funciones
+    
+    // Funciones sin parámetros
+    // Funciones con parámetros
+    // Funciones anónimas    
